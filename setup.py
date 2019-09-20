946bfff00313148cb5301d78caaf7da9ed7d2bb6
René Dudfield <renesd@gmail.com>
2019-09-20 19:34:35 +0200

    0.0.18

--- setup.py
+++ setup.py
@@ -100,7 +100,7 @@ setup(
         "thorpy",
         "pymunk>=5.4.2",
     ],
-    version='0.0.17',
+    version='0.0.18',
     entry_points={
         'console_scripts': [
             'stuntcat=stuntcat.cli:main',
